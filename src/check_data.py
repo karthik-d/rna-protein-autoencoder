d6a3a4e7d077b66f91cf89cd05b289d88e144798
Karthik Desingu <karthik19047@cse.ssn.edu.in>
2023-11-28 13:44:53 -0500

    utils and blocks

--- src/check_data.py
+++ src/check_data.py
@@ -1,1 +1,15 @@
+import os
+import scanpy as sc
+
+
 DATA_PATH = "../data"
+
+CITE_DATA_PATH = os.path.join(DATA_PATH, "covid-flu_HC_D0_selectedCellTypes_CITE.h5ad")
+RNA_DATA_PATH = os.path.join(DATA_PATH, "covid-flu_HC_D0_selectedCellTypes_RNA.h5ad")
+
+
+cite_df = sc.read_h5ad(CITE_DATA_PATH).to_df()
+print(cite_df.head())
+
+rna_df = sc.read_h5ad(RNA_DATA_PATH).to_df()
+print(rna_df.head())
