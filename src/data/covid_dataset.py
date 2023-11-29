b7ecda6094673f53e6e5e2c18c730c91738be228
Karthik Desingu <karthik19047@cse.ssn.edu.in>
2023-11-29 13:16:30 -0500

    save model

--- src/data/covid_dataset.py
+++ src/data/covid_dataset.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset
 import torch
 import os
 import scanpy as sc
+import numpy as np
 
 
 class CovidDataset(Dataset):
@@ -15,29 +16,27 @@ class CovidDataset(Dataset):
 		print("Protein Data.")
 		protein_data = sc.read_h5ad(PROTEIN_DATA_PATH)
 		print(protein_data.n_obs, protein_data.n_vars)
-		print(protein_data.obs_names)
 		print(protein_data.to_df())
 
 		print("RNA Data.")
 		rna_data = sc.read_h5ad(RNA_DATA_PATH)
 		print(rna_data.n_obs, rna_data.n_vars)
-		print(rna_data.obs_names)
 		print(rna_data.to_df())
 
-		## TODO: Divide based on split.
-		self.protein_split = protein_data.to_df()
-		self.rna_split = rna_data.to_df()
+		## TODO: Divide based on split (stratified).
+		self.protein_split = protein_data.to_df().iloc[:10000, :]
+		self.rna_split = rna_data.to_df().iloc[:10000, :]
 
 
 	def __len__(self):
-		return len(self.landmarks_frame)
+		return self.rna_split.shape[0]
 	
 
 	def __getitem__(self, idx):
 		if torch.is_tensor(idx):
 			idx = idx.tolist()
 
-		x_data = self.rna_split.iloc[idx, :]
-		y_data = self.protein_split.iloc[idx, :]
+		x_data = np.array(self.rna_split.iloc[idx, :]).astype(np.float32)
+		y_data = np.array(self.protein_split.iloc[idx, :]).astype(np.float32)
 
 		return (x_data, y_data)
--- src/train.py
+++ src/train.py
@@ -1,32 +1,76 @@
 import torch
 
 from nn.simple_autoencoder import AutoEncoder
+from data.covid_dataset import CovidDataset
 
+
+print("GPU Access:", torch.cuda.is_available())
+epoch_model_path = "../data/epoch-{epoch}_mse-{mse}_loss-{loss}.pth"
 
 # Model Initialization
-device = 'cuda:0'
+device = 'cpu'
+# device = 'cuda:0'
 model = AutoEncoder().to(device)
  
 # Validation using MSE Loss function
-loss_function = torch.nn.MSELoss()
+loss_function = torch.nn.CrossEntropyLoss()
+mse_function = torch.nn.MSELoss()
  
-# Using an Adam Optimizer with lr = 0.1
-optimizer = torch.optim.Adam(model.parameters(),
-                             lr = 1e-1,
-                             weight_decay = 1e-8)
+# Using an Adam Optimizer.
+optimizer = torch.optim.Adam(
+	model.parameters(),
+    lr = 1e-3,
+    weight_decay = 1e-4
+)
 
 
-num_epochs = 100
+def get_data_loader():
+	
+	dataset = CovidDataset(split='train')
+	# wrap dataset into dataloader.
+	return torch.utils.data.DataLoader(
+		dataset,
+		batch_size=32,
+		shuffle=True
+	)
+
+
+num_epochs = 50
 output =[]
+data_loader = get_data_loader()
 for epoch in range(num_epochs):
-    for data in loader:
-        img, _ = data
-        img = img.reshape(-1,64*64)
-        img    = img.to(device)
-        recon = model(img)                             
-        loss   = loss_function(recon, img.data)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    print(f'epoch [{epoch + 1}/{num_epochs}], loss:{loss. Item(): .4f}')
-    output.append((epoch,img,recon))
+	print(f"{epoch+1} of {num_epochs} ...")
+
+	loss_l = []
+	mse_l = []
+	for data in data_loader:
+		rna, protein = data
+
+		# to gpu.
+		rna = rna.to(device)
+		protein_recon = model(rna)
+
+		# compute loss.                            
+		loss = loss_function(protein_recon, protein)
+		loss_l.append(loss)
+
+		# compute mse.
+		mse = mse_function(protein_recon, protein)
+		mse_l.append(mse)
+		
+		# backpropagate. 
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+		
+	
+	# compute metrics.
+	avg_mse = sum(mse_l)/len(mse_l)
+	avg_loss = sum(loss_l)/len(loss_l)
+	print(f'Epoch [{epoch + 1}/{num_epochs}], loss:{avg_loss}, MSE:{avg_mse}')
+	output.append((epoch, sum(loss_l)/len(loss_l), sum(mse_l)/len(mse_l)))
+	
+	# save current model --> replaced at each epoch.
+	print("saving model state ...")
+	torch.save(model.state_dict(), epoch_model_path.format(epoch=epoch, loss=avg_loss, mse=avg_mse))
+    
