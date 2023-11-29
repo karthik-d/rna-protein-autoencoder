0dc85bef79e28ead2f5cd8fd9e8cb9dccecb49bc
Karthik Desingu <karthik19047@cse.ssn.edu.in>
2023-11-29 14:01:04 -0500

    stratified data split

--- src/check_data.py
+++ src/check_data.py
@@ -10,12 +10,11 @@ RNA_DATA_PATH = os.path.join(DATA_PATH, "covid-flu_HC_D0_selectedCellTypes_RNA.h
 
 cite_df = sc.read_h5ad(CITE_DATA_PATH)
 print(cite_df.n_obs, cite_df.n_vars)
-print(cite_df.obs_names)
-print(cite_df.to_df())
+print(cite_df.obs)
 # print(cite_df.head())
 
 rna_df = sc.read_h5ad(RNA_DATA_PATH)
 print(rna_df.n_obs, rna_df.n_vars)
 print(rna_df.obs_names)
-print(rna_df.to_df())
+print(rna_df.layers)
 # print(rna_df.head())
--- src/data/covid_dataset.py
+++ src/data/covid_dataset.py
@@ -1,4 +1,6 @@
 from torch.utils.data import Dataset
+from sklearn.model_selection import StratifiedShuffleSplit
+import pandas as pd
 import torch
 import os
 import scanpy as sc
@@ -12,6 +14,9 @@ class CovidDataset(Dataset):
 		DATA_PATH = "../data"
 		PROTEIN_DATA_PATH = os.path.join(DATA_PATH, "covid-flu_HC_D0_selectedCellTypes_CITE.h5ad")
 		RNA_DATA_PATH = os.path.join(DATA_PATH, "covid-flu_HC_D0_selectedCellTypes_RNA.h5ad")
+		
+		TRAIN_SAMPLES_PATH = os.path.join(DATA_PATH, "covid-flu_train-samples.csv")
+		TEST_SAMPLES_PATH = os.path.join(DATA_PATH, "covid-flu_test-samples.csv")
 
 		print("Protein Data.")
 		protein_data = sc.read_h5ad(PROTEIN_DATA_PATH)
@@ -23,9 +28,32 @@ class CovidDataset(Dataset):
 		print(rna_data.n_obs, rna_data.n_vars)
 		print(rna_data.to_df())
 
+		# split data, if not already done.
+		if not os.path.isfile(TRAIN_SAMPLES_PATH) or not os.path.isfile(TEST_SAMPLES_PATH):
+
+			print("Splitting data (stratified 80-20).")
+			splitter = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
+			train_indices, test_indices = next(
+				splitter.split(protein_data.obs, protein_data.obs['coarse.cell.type'])
+			)
+
+			train_rows = protein_data.to_df().iloc[train_indices].index.to_series(name='samples')
+			train_rows.to_csv(TRAIN_SAMPLES_PATH, index=False)
+
+			test_rows = protein_data.to_df().iloc[test_indices].index.to_series(name='samples')
+			test_rows.to_csv(TEST_SAMPLES_PATH, index=False)
+		
+		else:
+			train_rows = pd.read_csv(TRAIN_SAMPLES_PATH)['samples']
+			test_rows = pd.read_csv(TEST_SAMPLES_PATH)['samples']
+
 		## TODO: Divide based on split (stratified).
 		self.protein_split = protein_data.to_df().iloc[10000:20000, :]
 		self.rna_split = rna_data.to_df().iloc[10000:20000, :]
+
+		reqd_rows = train_rows if split=='train' else test_rows
+		self.protein_split = protein_data.to_df().loc[reqd_rows]
+		self.rna_split = rna_data.to_df().loc[reqd_rows]
 
 
 	def __len__(self):
